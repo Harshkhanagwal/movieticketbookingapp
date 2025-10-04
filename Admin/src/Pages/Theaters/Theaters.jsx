645666b576beccf493cbfb973acbfc0bdd00d9c3
dabbu-17 <david.singh.3683@gmail.com>
2025-10-04 22:37:58 +0530

    Theater list layout

--- Admin/src/Pages/Theaters/Theaters.jsx
+++ Admin/src/Pages/Theaters/Theaters.jsx
@@ -12,39 +12,72 @@ const Theaters = () => {
 
 
     return (
-        <div className="page-header">
 
-            <h1>Manage Theaters</h1>
-            <button onClick={() => setPopup(!popup)} ><MdOutlineAddToQueue className='app-icon' /> Add New Theater </button>
+        <>
+            <div className="page-header">
 
+                <h1>Manage Theaters</h1>
+                <button onClick={() => setPopup(!popup)} ><MdOutlineAddToQueue className='app-icon' /> Add New Theater </button>
+                {
+                    popup && (
+                        <>
 
-            {
-                popup && (
-                    <>
+                            <div className="popuparea">
+                                <div className="popupbox">
+                                    <div className="popupheader">
 
-                        <div className="popuparea">
-                            <div className="popupbox">
-                                <div className="popupheader">
+                                        <h3>Add new Theater</h3>
+                                        <IoClose onClick={() => setPopup(!popup)} className='app-icon popup-close-btn' />
 
-                                    <h3>Add new Theater</h3>
-                                    <IoClose onClick={() => setPopup(!popup)} className='app-icon popup-close-btn' />
+                                    </div>
 
+                                    <form className='add-theater-form'>
+
+                                        <input type="text" placeholder='Theater Name' />
+                                        <input type="text" placeholder='Theater Address' />
+                                        <input type="url" placeholder='Logo URL' />
+                                        <button type="submit" > Submit </button>
+                                    </form>
                                 </div>
-
-                                <form className='add-theater-form'>
-
-                                    <input type="text" placeholder='Theater Name' />
-                                    <input type="text" placeholder='Theater Address' />
-                                    <input type="url" placeholder='Logo URL' />
-                                    <button type="submit" > Submit </button>
-                                </form>
                             </div>
-                        </div>
-                    </>
-                )
-            }
+                        </>
+                    )
+                }
 
-        </div>
+            </div>
+
+            <div className="theaters-list">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>logo</th>
+                            <th>Name </th>
+                            <th>Avialable screens</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>
+                            <div className='theater-logo'>
+                            <img src="https://animationxpress.com/wp-content/uploads/2018/06/PVR-logo.png" alt="Theater Logo" style={{ width: '60px', height: '40px', objectFit: 'cover', borderRadius: '6px' }} />
+                            </div>
+                        </td>
+                        <td>PVR <span className='view-btn'>👁️</span></td>
+                        <td>2</td>
+                        <td>
+                            <div className='theater-actions'>
+                            <button className='edit-btn'>Edit</button>
+                            <button className='delete-btn'>Delete</button>
+                            </div>
+                        </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </>
+
     )
 }
 
